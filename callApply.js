d2741516057acfcbb4a46cdef6276f7d5dce7ea0
unknown <you@example.com>
2021-02-05 16:04:36 +0600

    bind,call,apply

--- callApply.js
+++ callApply.js
@@ -5,9 +5,9 @@ const normalPersn = {
     getFulName: function () {
         console.log(this.firstName, this.lastName);
     },
-    chargeBill: function (amount) {
+    chargeBill: function (amount,tips,tax) {
         console.log(this);
-        this.salary = this.salary - amount;
+        this.salary = this.salary - amount-tips-tax;
         return this.salary;
     }
 }
@@ -26,14 +26,25 @@ const friendlyPerson = {
     salary: 25000
 }
 //normalPersn.chargeBill();
-const heroChargeBill=normalPersn.chargeBill.bind(heroPerson);
-heroChargeBill(2000);
-heroChargeBill(3000);
+// const heroChargeBill=normalPersn.chargeBill.bind(heroPerson);
+// heroChargeBill(2000);
+// heroChargeBill(3000);
+// console.log(heroPerson.salary);
+// console.log(normalPersn.salary);
+
+
+// const friendlyChargeBill=normalPersn.chargeBill.bind(friendlyPerson);
+// friendlyChargeBill(1500);
+// friendlyChargeBill(1500);
+
+// normalPersn.chargeBill.call(heroPerson,900,100,10);
+// normalPersn.chargeBill.call(heroPerson,3000,300,30);
+// console.log(heroPerson.salary);
+
+// normalPersn.chargeBill.call(friendlyPerson,5000,500,50);
+// console.log(friendlyPerson.salary);
+// console.log(normalPersn.salary);
+
+normalPersn.chargeBill.apply(heroPerson,[3000,300,30]);
+normalPersn.chargeBill.apply(heroPerson,[4000,400,40]);
 console.log(heroPerson.salary);
-console.log(normalPersn.salary);
-
-
-
-const friendlyChargeBill=normalPersn.chargeBill.bind(friendlyPerson);
-friendlyChargeBill(1500);
-friendlyChargeBill(1500);
